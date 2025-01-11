6eef2d0d7985c5b6d47a6ec8f3d8be338ae8f472
Shan Huang <shan800208@gmail.com>
2025-01-11 11:20:27 +0800

    OpenNet homework upload

---
+++ Library/WebElements.py
@@ -0,0 +1,25 @@
+Cathay_Xpath = {
+    'Menu' : '//a[@class="cubre-a-burger"]',
+    '產品介紹' : '//div[@class="cubre-o-nav__content"]//*[contains(text(),"產品介紹")]',
+    '信用卡' : '//div[@class="cubre-o-menuLinkList__btn"]//*[contains(text(),"信用卡")]',
+    '掛失信用卡' : '//*[contains(text(),"掛失信用卡")]',
+    'Card Service list' : '/html/body/div[1]/header/div/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div/div[1]/div[2]/a',
+    '卡片介紹' : '//a[contains(text(),"卡片介紹")]',
+    '已停發' : '//*[contains(text(), "已停發")]',
+    '已停發圖片連結' : '//div[contains(text(), "已停發")]/..//img'
+    }
+
+Twitch_Xpath = {
+    'Click Browse' : '//a[@href="/directory"]//div[contains(text(), "瀏覽")]',
+    'Search input field' : '//header[@id="twilight-sticky-header-root"]//input[@placeholder="搜尋"]',
+    'StarCraft II icon' : '//img[@alt="StarCraft II"]',
+    'StarCraft II title' : '//p[@title="StarCraft II"]',
+    'Follow button' : '//button[@data-a-target="game-directory-follow-button"]',
+    '3rd video pic' : '//main[@id="page-main-content-wrapper"]//div[@role="list"]/div[3]//img[@class="tw-image"]',
+    'h1 title' : '//h1[contains(text(), "StarCraft II")]',
+    'Description' : '//main[@id="page-main-content-wrapper"]//p//span',
+    '4th video pic' : '//*[@id="page-main-content-wrapper"]/div[3]/div/div/div[4]//img[@class="tw-image"]',
+    '5th video pic' : '//*[@id="page-main-content-wrapper"]/div[3]/div/div/div[5]//img[@class="tw-image"]',
+    'Video Controller' : '//section[@id="channel-player"]',
+    'Check controller is hidden' : '//section[@id="channel-player"]/parent::div[@data-a-visible="false"]'
+}
